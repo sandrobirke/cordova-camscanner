4b78afdb976b0fd55701d4d805de2aba67e56302
dagiguevara <dagiguevara@gmail.com>
2016-05-03 17:49:00 -0600

    adding dynamic property

--- src/ios/AppDelegate+camscanner.m
+++ src/ios/AppDelegate+camscanner.m
@@ -13,6 +13,8 @@
 
 @implementation AppDelegate (camscanner)
 
+@dynamic viewController;
+
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
 {
    if ([CamScannerOpenAPIController isSourceApplicationCamScanner:sourceApplication])
