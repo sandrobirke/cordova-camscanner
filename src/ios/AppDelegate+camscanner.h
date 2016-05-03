955ddfd202be007c2f6ed51db84265b767ff8900
dagiguevara <dagiguevara@gmail.com>
2016-05-03 14:54:40 -0600

    adding header

--- src/ios/AppDelegate+camscanner.h
+++ src/ios/AppDelegate+camscanner.h
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "CordovaCamscanner.h"
 
 @interface AppDelegate(camscanner)
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation;
