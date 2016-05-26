cc01b5a45a3fa36a781da0f2977fdcfd32966bbd
Juan Jose Guevara <juanjo_guevara@hotmail.com>
2016-05-25 19:11:52 -0600

    Update AppDelegate+camscanner.m

--- src/ios/AppDelegate+camscanner.m
+++ src/ios/AppDelegate+camscanner.m
@@ -15,15 +15,20 @@
 
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
 {
-   
-       CordovaCamscanner *camscanner = [self.viewController getCommandInstance:@"CordovaCamscanner"];
-       [camscanner returnBase64:@"qwerty2"];
+   UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:nil message:@"app delegate" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    [alertView show];
    if ([CamScannerOpenAPIController isSourceApplicationCamScanner:sourceApplication])
    {
+   UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:nil message:@"is source application ok" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    [alertView show];
        NSDictionary *userInfo = [CamScannerOpenAPIController userInfoFromURL:url andSourceApplication:sourceApplication];
        NSString *fileFormat = [userInfo objectForKey:kReturnFileType];
        NSData *data = [CamScannerOpenAPIController getJPEGDataFromCamScannerWithUserInfo:userInfo];
        NSString *encodedString = [data base64EncodedStringWithOptions:0];
+       UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:nil message:@"sending result" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    [alertView show];
+    CordovaCamscanner *camscanner = [self.viewController getCommandInstance:@"CordovaCamscanner"];
+       [camscanner returnBase64:encodedString];
    }
    return YES;
 }
