6049a5761aa20fea13a6c7fe61a1f70c7089cbcb
Juan Jose Guevara <juanjo_guevara@hotmail.com>
2016-05-25 23:55:14 -0600

    Update CordovaCamscanner.h

--- src/ios/CordovaCamscanner.h
+++ src/ios/CordovaCamscanner.h
@@ -1,11 +1,20 @@
 #import <Cordova/CDVPlugin.h>
 #import <Cordova/CDV.h>
 
-@interface CordovaCamscanner : CDVPlugin <UIApplicationDelegate>
+@interface CordovaCamscanner : CDVPlugin
 
 @property (strong, nonatomic) CDVInvokedUrlCommand *command;
 
 - (void)scan: (CDVInvokedUrlCommand*)mycommand;
 - (void) returnBase64: (NSString*) base64EncodedString;
+
+@end
+
+@interface CordovaCamscannerStaticService : NSObject
+
++(CordovaCamscannerStaticService*) instance;
+
+@property (strong, nonatomic) CDVInvokedUrlCommand* command;
+@property (strong, nonatomic) CDVPlugin* plugin;
 
 @end
