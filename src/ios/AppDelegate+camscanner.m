6da2952f70c94389091caebd0a24d0979f7cee37
Juan Jose Guevara <juanjo_guevara@hotmail.com>
2016-05-25 23:54:50 -0600

    Update AppDelegate+camscanner.m

--- src/ios/AppDelegate+camscanner.m
+++ src/ios/AppDelegate+camscanner.m
@@ -11,26 +11,19 @@
 #import "CordovaCamscanner.h"
 #import <CamScannerOpenAPIFramework/CamScannerOpenAPIController.h>
 
-@implementation AppDelegate (camscanner)
+@implementation AppDelegate(camscanner)
 
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
 {
-   UIAlertView *alertView6 = [[UIAlertView alloc] initWithTitle:nil message:@"app delegate" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
-    [alertView6 show];
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:nil message:@"App Delegate" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    [alertView show];
    if ([CamScannerOpenAPIController isSourceApplicationCamScanner:sourceApplication])
    {
-   UIAlertView *alertView7 = [[UIAlertView alloc] initWithTitle:nil message:@"is source application ok" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
-    [alertView7 show];
        NSDictionary *userInfo = [CamScannerOpenAPIController userInfoFromURL:url andSourceApplication:sourceApplication];
-       NSString *fileFormat = [userInfo objectForKey:kReturnFileType];
        NSData *data = [CamScannerOpenAPIController getJPEGDataFromCamScannerWithUserInfo:userInfo];
        NSString *encodedString = [data base64EncodedStringWithOptions:0];
-       UIAlertView *alertView8 = [[UIAlertView alloc] initWithTitle:nil message:@"sending result" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
-    [alertView8 show];
-    CordovaCamscanner *camscanner = [self.viewController getCommandInstance:@"CordovaCamscanner"];
-    UIAlertView *alertView9 = [[UIAlertView alloc] initWithTitle:nil message:camscanner.command.callbackId delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
-       [alertView9 show];
-       [camscanner returnBase64:encodedString];
+       CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:encodedString];
+       [[CordovaCamscannerStaticService instance].plugin.commandDelegate sendPluginResult:pluginResult callbackId:[CordovaCamscannerStaticService instance].command.callbackId];
    }
    return YES;
 }
