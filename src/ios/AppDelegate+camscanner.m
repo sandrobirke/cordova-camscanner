aff74077783a6eadfcc54a5aaac96fe0f8c7e2d8
Juan Jose Guevara <juanjo_guevara@hotmail.com>
2016-05-25 18:44:37 -0600

    Update AppDelegate+camscanner.m

--- src/ios/AppDelegate+camscanner.m
+++ src/ios/AppDelegate+camscanner.m
@@ -15,15 +15,15 @@
 
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
 {
-   [camscanner returnBase64:@"qwerty2"];
+   
+       CordovaCamscanner *camscanner = [self.viewController getCommandInstance:@"CordovaCamscanner"];
+       [camscanner returnBase64:"qwerty2"];
    if ([CamScannerOpenAPIController isSourceApplicationCamScanner:sourceApplication])
    {
        NSDictionary *userInfo = [CamScannerOpenAPIController userInfoFromURL:url andSourceApplication:sourceApplication];
        NSString *fileFormat = [userInfo objectForKey:kReturnFileType];
        NSData *data = [CamScannerOpenAPIController getJPEGDataFromCamScannerWithUserInfo:userInfo];
        NSString *encodedString = [data base64EncodedStringWithOptions:0];
-       CordovaCamscanner *camscanner = [self.viewController getCommandInstance:@"CordovaCamscanner"];
-       [camscanner returnBase64:encodedString];
    }
    return YES;
 }
