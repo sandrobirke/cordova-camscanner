0f4bbf2fa73e20793ded5b1dd39762851f2127ba
Juan Jose Guevara <juanjo_guevara@hotmail.com>
2016-10-21 17:09:37 -0600

    Update CordovaCamscanner.java

--- src/android/CordovaCamscanner.java
+++ src/android/CordovaCamscanner.java
@@ -1,123 +1,62 @@
-package com.megster.cordova;
+package com.beelphegor.cordovacamscanner;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-import android.util.Base64;
-import android.util.Log;
-import android.webkit.MimeTypeMap;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+public class CordovaCamscanner extends CordovaPlugin {
+    CallbackContext _callbackContext;
 
-public class FileChooser extends CordovaPlugin {
-
-    private static final String TAG = "FileChooser";
-    private static final String ACTION_OPEN = "open";
-    private static final int PICK_FILE_REQUEST = 1;
-    private String RESPONSE_TYPE;
-    CallbackContext callback;
-
-    @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        RESPONSE_TYPE = args.getString(0).toLowerCase();
-        if (action.equals(ACTION_OPEN)) {
-            chooseFile(callbackContext);
-            return true;
-        }
+        _callbackContext = callbackContext;
 
+        try{
+            if(action.equals("scan")) {
+                String srcUri = args.getString(0).replace("file://", "");
+                validateInputs(srcUri);
+                Context context = this.cordova.getActivity().getApplicationContext();
+                Intent intent = new Intent(context, CamscannerActivity.class);
+                intent.putExtra("SRC_URI", srcUri);
+                cordova.setActivityResultCallback (this);
+                this.cordova.getActivity().startActivityForResult(intent, 2);
+                return true;
+            }
+        }catch (Exception e){
+            callbackContext.error(e.getMessage());
+            return false;
+        }
         return false;
     }
 
-    public void chooseFile(CallbackContext callbackContext) {
-
-        // type and title should be configurable
-
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("*/*");
-        String[] mimetypes = {"image/*", "application/pdf"};
-        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes);
-        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-
-        Intent chooser = Intent.createChooser(intent, "Select File");
-        cordova.startActivityForResult(this, chooser, PICK_FILE_REQUEST);
-
-        PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-        pluginResult.setKeepCallback(true);
-        callback = callbackContext;
-        callbackContext.sendPluginResult(pluginResult);
-    }
-
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if (requestCode == PICK_FILE_REQUEST && callback != null) {
-
-            if (resultCode == Activity.RESULT_OK) {
-
-                Uri uri = data.getData();
-
-                if (uri != null) {
-
-                    Log.w(TAG, uri.toString());
-                    try {
-                        File f = new File(uri.getPath());
-                        JSONObject json = new JSONObject();
-                        if(RESPONSE_TYPE.equals("base64")){
-                            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-                            InputStream is = new FileInputStream(f);
-                            byte[] temp = new byte[1024];
-                            int read;
-
-                            while((read = is.read(temp)) >= 0){
-                                buffer.write(temp, 0, read);
-                            }
-
-                            byte[] byteArray = buffer.toByteArray();
-                            String encodedFile= Base64.encodeToString(byteArray, Base64.DEFAULT);
-                            json.put("encodedFile", encodedFile);
-
-                        } else {
-                            json.put("uri", uri.toString());
-                        }                        
-                        json.put("extension", MimeTypeMap.getFileExtensionFromUrl(f.toURI().toURL().toString()));
-                        callback.success(json);
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                } else {
-
-                    callback.error("File uri was null");
-
-                }
-
-            } else if (resultCode == Activity.RESULT_CANCELED) {
-
-                // TODO NO_RESULT or error callback?
-                PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-                callback.sendPluginResult(pluginResult);
-
-            } else {
-
-                callback.error(resultCode);
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == 2){
+            if(data.getStringExtra("RESULT").equals("success")) {
+                _callbackContext.success(data.getStringExtra("BASE64_RESULT"));
+            }
+            if(data.getStringExtra("RESULT").equals("error")) {
+                _callbackContext.error(data.getStringExtra("ERROR"));
+            }
+            if(data.getStringExtra("RESULT").equals("cancel")) {
+                _callbackContext.error("canceled");
             }
         }
+    }
+
+    public void validateInputs(String srcUri) throws InvalidInputException {
+        if(srcUri == null || srcUri.isEmpty()){
+            throw new InvalidInputException("invalid value for parameter: srcUri");
+        }
+    }
+
+}
+
+class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
     }
 }
