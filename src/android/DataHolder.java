e438083a2162818078e8d115afa3e9686473a907
Erick Caballero <erickson_523@hotmail.com>
2017-04-13 14:23:28 -0600

    Created a class to send the enconded image

---
+++ src/android/DataHolder.java
@@ -0,0 +1,10 @@
+package com.beelphegor.cordovacamscanner;
+
+public class DataHolder {
+    private String data;
+    public String getData() {return data;}
+    public void setData(String data) {this.data = data;}
+
+    private static final DataHolder holder = new DataHolder();
+    public static DataHolder getInstance() {return holder;}
+}
